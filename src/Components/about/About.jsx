c08440833bba71b83156e75432921466cf4738f0
Akshay Jha <akshay0127.be21@chitkara.edu.in>
2022-10-11 10:37:33 +0530

    Media Query Added

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import Me from '../../assets/ak.png'
+import Me from '../../assets/ak.jpg'
 import { FaAward } from 'react-icons/fa'
 import { FiUsers } from 'react-icons/fi'
 import { VscFolderLibrary } from 'react-icons/vsc'
